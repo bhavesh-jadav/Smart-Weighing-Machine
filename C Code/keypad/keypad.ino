d2e290825aa7fad5a16e29caf370ca4bfcd58b36
Bhavesh Jadav <bhaveshhjadav@gmail.com>
2017-01-01 17:14:13 +0530

    Modified keypade.ino file

--- C Code/keypad/keypad.ino
+++ C Code/keypad/keypad.ino
@@ -15,18 +15,20 @@
  * 1) assign multiple characters to single button(which I refered to as key in this program) and cycle through them one by one by pressing same button in some time interval.
  * 2) assign multiple strings to single button(which I refered to as cmd in this program) and cycle through them one by one by pressing same button in some time interval.
  *    note that this strings can be made of single characters to multiple characters. 
- * 3) assign long press functionality to the button whcich can only have 
+ * 3) assign long press i.e. hold button functionality any of the above case
  */
 
 #include <Keypad.h>
 #include <String.h>
 
+//change following according to the keypad
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //three columns
-
 byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {19, 18, 17, 16}; //connect to the column pinouts of the keypad
 
+//assign number to the each key as shwon in below. since I am using 4x4 keypad hence numbers are from 0 to 15.
+//MAKE SURE NUMBERING STARTS FROM 0 AND THERE IS NO NUMBERS ARE MISSING BETWEEN TWO NUMBERS!
 char keys[ROWS][COLS] = {
     { 0,1,2,3 },
     { 4,5,6,7 },
@@ -34,9 +36,12 @@ char keys[ROWS][COLS] = {
     { 12,13,14,15 }
 };
 
-Keypad key_pad(makeKeymap(keys), rowPins, colPins, sizeof(rowPins), sizeof(colPins));
-int keyCounterArray[16] = {0};
-//here '-' will indicate end of command and null character will indicate end of string.
+/*
+ * Following array is used to assign characters and strings to the keys
+ * 1) If you want to assign character to the keys then assign characters without space for e.g. '2abcABC'
+ * 2) If you want to assign strings to keys then assign strings with '-' at the end for e.g. 'caps-small-no'
+ * 3) If you want to assign both character and string then you can do it like 'A-a-hello-*-ok'. by adding '-' between characters and strings.
+ */
 char* keyStringArray[20] = {
   "1.", "2abcABC", "3defDEF", "caps-small-no-",
   "4ghiGHI", "5jklJKL", "6mnoMNO", "up-",
@@ -44,6 +49,10 @@ char* keyStringArray[20] = {
   "left-", "0", "right-", "tare-"
 };
 
+/*
+ * if you want to assign long press button functionality to any button then assign function using following array.
+ * make sure to assign to appropriate button.
+ */
 char* longPressKeyStringArray[20] = {
   "", "", "", "",
   "", "", "", "",
@@ -52,18 +61,24 @@ char* longPressKeyStringArray[20] = {
 };
 
 //999 to indicate end of array
-int alphaKeys[] = {1, 2, 4, 5, 6, 8, 9, 10, 999};
-int singlePressKeyButtons[] = {0, 13, 999};
-int singlePressCmdButtons[] = {3, 7, 11, 12, 14, 15, 999};
-int longPressCmdButtons[] = {12, 14, 15, 999};
+int alphaKeys[] = {1, 2, 4, 5, 6, 8, 9, 10, 999};  //add numbers of keys which are going to represent alphabet.
+int singlePressKeyButtons[] = {0, 1, 2, 4, 5, 6, 8, 9, 10, 13, 999};  //add numbers of keys which are going to cycle through characters
+int singlePressCmdButtons[] = {3, 7, 11, 12, 14, 15, 999}; //add numbers of keys which are going to cycle through strings
+int longPressCmdButtons[] = {12, 14, 15, 999};  //add nubers of characters which are going to have long press button functionality
 
-static byte kpadState;
-char textMode[10] = "no";
+static byte kpadState;  //to store key state i.e. PRESSED, HOLD or RELEASED
+char textMode[10] = "no";  //initial text mode, no = number, caps = capital alphabet, small = small alphabet
 byte keyState = 0;  //0 = released, 1 = hold
-int textModeKey = 3;
-char valueToSend[50] = {};
+int textModeKey = 3; //assign textmode key number to this variable.
+char valueToSend[50] = {}; //it will store final value to send to the serial monitor.
 
 unsigned long TimeInMillis;
+
+//initialize the keypad
+Keypad key_pad(makeKeymap(keys), rowPins, colPins, sizeof(rowPins), sizeof(colPins));
+
+//follwoing array will used to store the number of times the key is pressed which will help to cycle through characters or strings.
+int keyCounterArray[16] = {0};
 
 void setup() {
   Serial.begin(9600);
@@ -78,10 +93,13 @@ void loop() {
   int val = key_pad.getKey();
 }
 
+//minimum time in milliseconds required between two key press to cycle through values
 int TimeDiffBtwnKeyPress = 1500;
 
-void getKeyFromKeyPress(int keyVal, int beginIndex, int endIndex){
-  
+//following function is used to cycle through charactes
+//startIndex and endIndex are used to cycle through particular part of character array. this is important because
+//in 'caps' text mode we only want to cycle through capital letters, in 'small' text mode we only want to cycle through small letters etc.
+void getKeyFromKeyPress(int keyVal, int beginIndex, int endIndex){  
   char subString[50] = {};
   strncpy(subString, keyStringArray[keyVal] + beginIndex, endIndex - beginIndex);
 
@@ -98,6 +116,7 @@ void getKeyFromKeyPress(int keyVal, int beginIndex, int endIndex){
   Serial.println(valueToSend);
 }
 
+//following function is used to cycle through strings
 void getCommandFromKeyPress(int keyVal){
   int counterLimit = 0;
   int i = 0, j = 0, k = 0;
@@ -128,7 +147,7 @@ void getCommandFromKeyPress(int keyVal){
   Serial.println(valueToSend);
 }
 
-//state 0 = released, 1 = hold
+//following function is used for long press functionality
 void getCommandFormLongKeyPress(int keyVal){
   if(longPressKeyStringArray[keyVal] != ""){
     strcpy(valueToSend, longPressKeyStringArray[keyVal]);
@@ -154,16 +173,15 @@ void keypadEvent(KeypadEvent key){
       if(keyState == 1)
         keyState = 0;
         
-      /*else if(isValueInArray(keyVal, longPressCmdButtons))
-        getCommandFormLongKeyPress(keyVal, keyState);*/
       else if(isValueInArray(keyVal, singlePressCmdButtons))
         getCommandFromKeyPress(keyVal);
-        
+      
+      //following part of code will use startIndex and endIndex of particular part of character array to cycle through on that much part.
       else if(isValueInArray(keyVal, alphaKeys) && strcmp(textMode, "caps") == 0)
         getKeyFromKeyPress(keyVal, (strlen(keyStringArray[keyVal])/2) + 1, strlen(keyStringArray[keyVal]));
       else if(isValueInArray(keyVal, alphaKeys) && strcmp(textMode, "small") == 0)
         getKeyFromKeyPress(keyVal, 1, strlen(keyStringArray[keyVal])/2 + 1);
-      else if((isValueInArray(keyVal, singlePressKeyButtons) || isValueInArray(keyVal, alphaKeys)) && strcmp(textMode, "no") == 0)
+      else if(isValueInArray(keyVal, singlePressKeyButtons) && strcmp(textMode, "no") == 0)
         getKeyFromKeyPress(keyVal, 0, 1);
         
       else if(isValueInArray(keyVal, singlePressKeyButtons))
