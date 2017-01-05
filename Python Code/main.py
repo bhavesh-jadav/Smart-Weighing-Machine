166cdb31d8c0fb7af66ed3d1e8137946074b059c
bhavesh-jadav <bhaveshhjadav@gmail.com>
2017-01-05 13:01:17 +0000

    modified loadcell.py and main..py

--- Python Code/main.py
+++ Python Code/main.py
@@ -2,17 +2,17 @@ import Modules.loadcell as loadcell
 from Modules import lcd_init as lcd
 import RPi.GPIO as GPIO
 import datetime
+import urllib2
 
 loadcell.init()
-#lcd.draw.text("0123456789", 0, 0, size = 10, font="/home/pi/Desktop/Smart-Weighing-Machine/Python Code/Modules/pyLCD/fonts/3x5.fnt")
-#lcd.display.commit()
+
+
 try:
 	while 1:
 		
 		date_and_time = str(datetime.date.today().strftime("%-d-%-m-%y ") + datetime.datetime.now().time().strftime("%-H:%-M"))
 		lcd.draw.text(date_and_time,127-len(date_and_time)*4,0,font="/home/pi/Desktop/Smart-Weighing-Machine/Python Code/Modules/pyLCD/fonts/3x5.fnt")
 		lcd.display.commit()
-		
 		
 		'''
 		lcd.display.clear()
